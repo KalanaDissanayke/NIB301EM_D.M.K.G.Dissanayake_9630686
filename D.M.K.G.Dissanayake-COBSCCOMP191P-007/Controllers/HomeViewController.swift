5a5a2f01cbfc7f112ec10673be2a9ca44505a673
KalanaDissanayke <kalana0773386244@gmail.com>
2020-09-18 19:48:23 +0530

    Added auth navigation

---
+++ D.M.K.G.Dissanayake-COBSCCOMP191P-007/Controllers/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  D.M.K.G.Dissanayake-COBSCCOMP191P-007
+//
+//  Created by Kalana Dissanayake on 9/18/20.
+//  Copyright © 2020 Kalana Dissanyake. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+        
+    }
+
+    override func viewDidAppear(_ animated: Bool){
+        tabBarController?.tabBar.isHidden = false
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
