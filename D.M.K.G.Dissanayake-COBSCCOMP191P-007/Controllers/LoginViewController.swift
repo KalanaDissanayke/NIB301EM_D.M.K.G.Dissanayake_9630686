d85dc268926fbf5a6fda4147a60b63c4582d063a
Tharindu Asintha <tharinduasintha1@gmail.com>
2020-09-18 19:48:23 +0530

    Added auth navigation

---
+++ D.M.K.G.Dissanayake-COBSCCOMP191P-007/Controllers/LoginViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoginViewController.swift
+//  D.M.K.G.Dissanayake-COBSCCOMP191P-007
+//
+//  Created by Kalana Dissanayake on 9/18/20.
+//  Copyright © 2020 Kalana Dissanyake. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidAppear(_ animated: Bool){
+        tabBarController?.tabBar.isHidden = true
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    
+    @IBAction func CloseButton(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 0
+    }
+}
